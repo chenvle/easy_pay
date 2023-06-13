183a61ada9538ed36ed80aafbf04c4e5ee47c62c
chenvle <415387649@qq.com>
2023-06-13 14:40:22 +0800

    初始化

--- src/EasyPay.php
+++ src/EasyPay.php
@@ -1,25 +1,16 @@
 <?php
 
+use EasyWeChat\Factory;
+
 class EasyPay extends Base
 {
-    /**
-     * @var array|string[]
-     */
-    protected $config;
 
-    public function __construct($config)
+    public function __construct($config = false)
     {
-        $this->config = [
-            // 必要配置
-            'app_id'             => '',
-            'mch_id'             => '',
-            'key'                => '',   // API v2 密钥 (注意: 是v2密钥 是v2密钥 是v2密钥)
-
-            // 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
-            'cert_path'          => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
-            'key_path'           => 'path/to/your/key',      // XXX: 绝对路径！！！！
-
-            'notify_url'         => '默认的订单回调地址',     // 你也可以在下单时单独设置来想覆盖它
-        ];
+        if($config){
+            $app = Factory::payment($config);
+        }else{
+            $app = Factory::payment(config('pay'));
+        }
     }
 }
